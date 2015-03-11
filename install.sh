cf1bd1b01ca3e2144421b67cd3a2b03b2768f83a
Sam Hewitt <hewittsamuel@gmail.com>
2015-03-11 01:58:14 -0400

    Updated install script

--- install.sh
+++ install.sh
@@ -22,13 +22,28 @@
 # this program; if not, see <https://www.gnu.org/licenses/lgpl-3.0.txt>
 
 clear
-echo '#---------------------------------------------#'
-echo '#     Paper Icon Theme Install Script         #'
-echo '#---------------------------------------------#'
+echo '#------------------------------------------#'
+echo '#     Paper Icon Theme Install Script      #'
+echo '#------------------------------------------#'
+
+
+show_question() {
+echo -e "\033[1;34m$@\033[0m"
+}
+
+show_dir() {
+echo -e "\033[1;32m$@\033[0m"
+}
+
+show_error() {
+echo -e "\033[1;31m$@\033[0m"
+}
 
 function continue {
-echo ""
-read -p "Do you want to continue? (Y)es, (N)o : " INPUT
+echo
+show_question '\tDo you want to continue? (Y)es, (N)o : ' 
+echo
+read INPUT
 case $INPUT in
 	[Yy]* ) main;;
     [Nn]* ) exit 99;;
@@ -40,43 +55,45 @@ function main {
 if [ "$UID" -eq "$ROOT_UID" ]; then
 	if [ -d /usr/share/icons/Paper ]; then
 		echo
-		read -p "Found an existing installation. Replace it? (Y)es, (N)o : " INPUT
+		show_question '\tFound an existing installation. Replace it? (Y)es, (N)o : ' 
+		echo
+		read INPUT
 		case $INPUT in
 			[Yy]* ) rm -Rf /usr/share/icons/Paper 2>/dev/null;;
 			[Nn]* );;
-		    * )
-		    clear && echo 'Sorry, try again.'
-		    main
-		    ;;
+		    * ) clear; show_error '\tSorry, try again.'; main;;
 		esac
 	fi
-	if [ -d /usr/share/icons/ ]; then
-		cp -R ./Paper/ /usr/share/icons/	
-		chmod -R 755 /usr/share/icons/Paper
-	else
-		mkdir -p  /usr/share/icons/
-		cp -R ./Paper/ /usr/share/icons/
-		chmod -R 755 /usr/share/icons/Paper
-	fi
+	echo "Installing..."
+	cp -R ./Paper/ /usr/share/icons/
+	chmod -R 755 /usr/share/icons/Paper
 	echo "Installation complete!"
-	set
+	end
 elif [ "$UID" -ne "$ROOT_UID" ]; then
 	if [ -d $HOME/.local/share/icons/Paper ]; then
 		echo
-		read -p "Found an existing installation. Replace it? (Y)es, (N)o : " INPUT
+		show_question '\tFound an existing installation. Replace it? (Y)es, (N)o : ' 
+		echo
+		read INPUT
 		case $INPUT in
 			[Yy]* ) rm -Rf "$HOME/.local/share/icons/Paper" 2>/dev/null;;
 			[Nn]* );;
-		    * )
-		    clear && echo 'Sorry, try again.'
-		    main
-		    ;;
+		    * ) clear; show_error '\tSorry, try again.'; main;;
 		esac
 	fi
-	if [ -d $HOME/.local/share/icons/Paper ]; then
+	echo "Installing..."
+	# .local/share/icons
+	if [ -d $HOME/.local/share/icons ]; then
 		cp -R ./Paper/ $HOME/.local/share/icons/
 	else
-		mkdir -p  $HOME/.local/share/icons/
+		mkdir -p $HOME/.local/share/icons
+		cp -R ./Paper/ $HOME/.local/share/icons/
+	fi
+	# ./icons
+	if [ -d $HOME/./icons ]; then
+		cp -R ./Paper/ $HOME/./icons/
+	else
+		mkdir -p $HOME/./icons
 		cp -R ./Paper/ $HOME/.local/share/icons/
 	fi
 	echo "Installation complete!"
@@ -84,18 +101,23 @@ elif [ "$UID" -ne "$ROOT_UID" ]; then
 fi
 }
 
+
+
 function set {
-echo ""
-read -p "Do you want to set Paper as desktop theme? (Y)es, (N)o : " INPUT
+echo
+show_question '\tDo you want to set Paper as your icon theme? (Y)es, (N)o : ' 
+echo
+read INPUT
 case $INPUT in
 	[Yy]* ) settheme;;
     [Nn]* ) end;;
-    * ) echo; echo "Uh oh, invalid response. Please retry."; set;;
+    * ) echo; show_error "\aUh oh, invalid response. Please retry."; set;;
 esac
 }
 
 function settheme {
 echo "Setting Paper as desktop Icon theme..."
+gsettings reset org.gnome.desktop.interface icon-theme
 gsettings set org.gnome.desktop.interface icon-theme "Paper"
 echo "Done."
 end
@@ -109,12 +131,18 @@ function end {
 ROOT_UID=0
 if [ "$UID" -ne "$ROOT_UID" ]; then
 	echo
-	echo "Paper Icon Theme will be installed in '$HOME/.local/share/icons/Paper'."
+	echo "Paper Icon Theme will be installed in:"
+	echo
+	show_dir '\t$HOME/.local/share/icons'
+	echo
 	echo "To make them available to all users, run this script as root."
 	continue
 else
 	echo
-	echo "Paper Icon Theme will be installed in '/usr/share/icons'"
+	echo "Paper Icon Theme will be installed in:"
+	echo
+	show_dir '\t/usr/share/icons'
+	echo
 	echo "It will be available to all users."
 	continue
 fi
